1e129bf7ed68c20c6500e4ffa10a4f94fe589729
Derick Chan <derickchan68@gmail.com>
2020-06-19 11:55:46 -0400

    Add files via upload
    Main (UI) Method as well as Java backend to provide game function as well as AI function

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+//Finished UI using Swing
+public class Main
+{
+    public static void main(String[] args)
+    {
+	TicTacToe game = new TicTacToe();
+    }
+}
